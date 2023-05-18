e4de67705c6ed4c3589b951a7f3b840d4fd73212
sharmaharsh897 <sharmaharsh897@gmail.com>
2023-05-18 14:38:35 +0530

    arranged the items in a proper format

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import './Card.css'
+
+function Card(props){
+    const classes='card '+props.className;
+return <div className={classes}>{props.children}</div>
+
+}
+
+export default Card;
+
+// import './Card.css';
+
+// function Card(props) {
+//   const classes = 'card ' + props.className;
+  
+//   return <div className={classes}>{props.children}</div>;
+// }
+
+// export default Card;
